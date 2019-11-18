f06af2ab324673d0128faae1f77b91e73673f88c
Jason Carcamo <jasoncarcamo30@yahoo.com>
2019-11-17 23:23:30 -0500

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace julis_cleaning.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
